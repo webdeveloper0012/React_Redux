7bd57ea6e09f29546e4f78259dd7bfc79302bac0
webdeveloper0012 <eneanako012@gmail.com>
2020-08-24 00:42:10 -0300

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,5 +16,5 @@ const Footer = () => (
     </FilterLink>
     </div>
 )
-
+ 
 export default Footer
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -14,7 +14,7 @@ export const toggleTodo = id => ({
     type: 'TOGGLE_TODO',
     id
 })
-
+ 
 export const VisibilityFilters = {
     SHOW_ALL: 'SHOW_ALL',
     SHOW_COMPLETED: 'SHOW_COMPLETED',
